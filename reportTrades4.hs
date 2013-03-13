8c2f68e545385223b5ec1f11fab807e78016e133
Derek Mahar <derek.mahar@intelerad.com>
2013-03-12 20:54:19 -0400

    Introduce Summary data type to replace triplet.

--- reportTrades4.hs
+++ reportTrades4.hs
@@ -1,10 +1,11 @@
-data Trade = Buy { unitPrice :: Double, quantity :: Double }
+data Trade = Buy {unitPrice :: Double, quantity :: Double} deriving (Show)
+data Summary = Summary {totalCost :: Double, totalQuantity :: Double, averageCost :: Double} deriving (Show)
 
-reportTrades :: [Trade] -> [(Double, Double, Double)]
-reportTrades xs = scanl reportTradesItem (0,0,0) xs
+reportTrades :: [Trade] -> [Summary]
+reportTrades xs = scanl reportTradesItem Summary {totalCost = 0, totalQuantity = 0, averageCost = 0} xs
 
-reportTradesItem :: (Double, Double, Double) -> Trade -> (Double, Double, Double)
-reportTradesItem (totalCost, totalQuantity, averageCost) trade =
-        let     newTotalCost = totalCost + unitPrice trade * quantity trade
-                newTotalQuantity = totalQuantity + quantity trade
-        in      (newTotalCost, newTotalQuantity, newTotalCost / newTotalQuantity)
+reportTradesItem :: Summary -> Trade -> Summary 
+reportTradesItem summary trade =
+        let     newTotalCost = totalCost summary + unitPrice trade * quantity trade
+                newTotalQuantity = totalQuantity summary + quantity trade
+        in      Summary {totalCost = newTotalCost, totalQuantity = newTotalQuantity, averageCost = newTotalCost / newTotalQuantity}
